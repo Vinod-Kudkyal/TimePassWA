bc93dc82ee1a30b00715722cb930973103f5ae62
vinodkudkyal <vinodkudkyal2005@gmail.com>
2024-04-11 22:41:45 +0530

    First Commit TPWA

---
+++ src/main/java/in/tp/Timepass1.java
@@ -0,0 +1,47 @@
+package in.tp;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class Timepass1
+ */
+@WebServlet("/Timepass1")
+public class Timepass1 extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see GenericServlet#GenericServlet()
+     */
+    public Timepass1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
